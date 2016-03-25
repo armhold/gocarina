c7156c2ddedeff42941038b6002f9eab34704471
George Armhold <armhold@gmail.com>
2016-03-24 23:36:23 -0400

    assignRandomWeights

--- network.go
+++ network.go
@@ -12,31 +12,51 @@ func init() {
 }
 
 type Network struct {
-	NumInputs     int // total of bits in the image
+	NumInputs     int         // total of bits in the image
+	NumOutputs    int
 	HiddenCount   int
 	InputValues   []int       // image bits
 	InputWeights  [][]float64 // weights from inputs -> hidden nodes
 	HiddenOutputs []float64   // after feed-forward, what the hidden nodes output
-	OutputWeights []float64   // weights from hidden nodes -> output nodes
+	OutputWeights [][]float64 // weights from hidden nodes -> output nodes
 	OutputValues  []float64   // after feed-forward, what the output nodes output
 	OutputErrors  []float64
 	HiddenErrors  []float64
 }
 
 func NewNetwork(charWidth, charHeight int) *Network {
-	return &Network{NumInputs: charWidth * charHeight, HiddenCount: 25}
+	return &Network{NumInputs: charWidth * charHeight, HiddenCount: 25, NumOutputs: 8}
 }
 
 func (n *Network) assignRandomWeights() {
 
+	// input -> hidden weights
+	//
 	for i := 0; i < n.NumInputs; i++ {
 		weights := make([]float64, n.HiddenCount)
 
 		for j := 0; j < len(weights); j++ {
-			weights[j] = rand.Float64()
+
+			// we want the overall sum of weights to be < 1
+			weights[j] = rand.Float64() / float64(n.NumInputs * n.HiddenCount)
 		}
 
 		n.InputWeights = append(n.InputWeights, weights)
+	}
+
+
+	// hidden -> output weights
+	//
+	for i := 0; i < n.HiddenCount; i++ {
+		weights := make([]float64, n.NumOutputs)
+
+		for j := 0; j < len(weights); j++ {
+
+			// we want the overall sum of weights to be < 1
+			weights[j] = rand.Float64() / float64(n.HiddenCount * n.NumOutputs)
+		}
+
+		n.OutputWeights = append(n.OutputWeights, weights)
 	}
 
 }
